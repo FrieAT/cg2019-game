83ef01632a6f6bf9859d4cc100c48360a7dc3e82
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2019-05-25 17:16:49 +0200

    Removed mode from fragment shader in SphereShader

--- 2019_cg_physics/2019_cg_physics/Components/Shader/SphereShader/SphereShader.cpp
+++ 2019_cg_physics/2019_cg_physics/Components/Shader/SphereShader/SphereShader.cpp
@@ -40,7 +40,6 @@ SphereShader::SphereShader()
        uniform mat4 view;
        uniform vec3 cameraPosition;
        uniform float shininess;
-       uniform int mode;
        uniform sampler2D textureData;
        
        vec3 lightPosition = vec3(0.0f, 4.0f, 0.0f);
@@ -56,49 +55,42 @@ SphereShader::SphereShader()
        
        out vec4 outColor;
        void main() {
-           if (mode == 0) // counter shall be constantly lighted (mode == 1)
-           {
-               //calculate normal in world coordinates
-               mat3 normalMatrix = transpose(inverse(mat3(anim)));
-               vec3 normal = normalize(normalMatrix * normalOut);
-               
-               //calculate the location of this fragment (pixel) in world coordinates
-               vec3 surfacePosition = vec3(anim * vec4(positionOut, 1));
-               
-               //calculate the vector from this pixels surface to the light source and the camera
-               vec3 surfaceToLight = normalize(lightPosition - surfacePosition);
-               vec3 surfaceToCamera = normalize(cameraPosition - surfacePosition);
-               
-               //calculate the cosine of the angle of incidence
-               float diffuseCoefficient = max(0.0, dot(normal, surfaceToLight));
-               
-               //calculate diffuse component
-               vec3 diffuse = diffuseCoefficient * lightIntensities * colorVtxOut;
-               
-               //calculate ambient component
-               vec3 ambient = ambientCoefficient * lightIntensities * colorVtxOut;
-               
-               //calculate specular component
-               float specularCoefficient = 0.0;
-               if (diffuseCoefficient > 0.0)
-                   specularCoefficient = pow(max(0.0, dot(surfaceToCamera, reflect(-surfaceToLight, normal))), shininess);
-               vec3 specular = specularCoefficient * specularColor * lightIntensities;
-               
-               //attenuation
-               float distanceToLight = length(lightPosition - surfacePosition);
-               float attenuation = 1.0 / (1.0 + lightAttenuation * pow(distanceToLight, 2));
-               
-               //linear color (color before gamma correction)
-               vec3 linearColor = ambient + attenuation * (diffuse + specular);
-               
-               //final color (after gamma correction)
-               vec3 gamma = vec3(1.0 / 2.2);
-               outColor = vec4(pow(linearColor, gamma), 1.0);
-           }
-           else if (mode == 1) // for the counter
-               outColor = vec4(colorVtxOut, 1.0f);
-           else // texture
-               outColor = texture(textureData, textureCoordOut);
+           //calculate normal in world coordinates
+           mat3 normalMatrix = transpose(inverse(mat3(anim)));
+           vec3 normal = normalize(normalMatrix * normalOut);
+           
+           //calculate the location of this fragment (pixel) in world coordinates
+           vec3 surfacePosition = vec3(anim * vec4(positionOut, 1));
+           
+           //calculate the vector from this pixels surface to the light source and the camera
+           vec3 surfaceToLight = normalize(lightPosition - surfacePosition);
+           vec3 surfaceToCamera = normalize(cameraPosition - surfacePosition);
+           
+           //calculate the cosine of the angle of incidence
+           float diffuseCoefficient = max(0.0, dot(normal, surfaceToLight));
+           
+           //calculate diffuse component
+           vec3 diffuse = diffuseCoefficient * lightIntensities * colorVtxOut;
+           
+           //calculate ambient component
+           vec3 ambient = ambientCoefficient * lightIntensities * colorVtxOut;
+           
+           //calculate specular component
+           float specularCoefficient = 0.0;
+           if (diffuseCoefficient > 0.0)
+               specularCoefficient = pow(max(0.0, dot(surfaceToCamera, reflect(-surfaceToLight, normal))), shininess);
+           vec3 specular = specularCoefficient * specularColor * lightIntensities;
+           
+           //attenuation
+           float distanceToLight = length(lightPosition - surfacePosition);
+           float attenuation = 1.0 / (1.0 + lightAttenuation * pow(distanceToLight, 2));
+           
+           //linear color (color before gamma correction)
+           vec3 linearColor = ambient + attenuation * (diffuse + specular);
+           
+           //final color (after gamma correction)
+           vec3 gamma = vec3(1.0 / 2.2);
+           outColor = vec4(pow(linearColor, gamma), 1.0);
        }
     );
 }
