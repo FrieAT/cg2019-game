71e2d781a641c7a8c806da4520ca65ce551f660e
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2019-05-26 16:02:39 +0200

    Re-defined CubeDrawing.

---
+++ 2019_cg_physics/2019_cg_physics/Components/Drawing/SteveDrawing/SteveDrawing.cpp
@@ -0,0 +1,9 @@
+//
+//  SteveDrawing.cpp
+//  2019_cg_physics
+//
+//  Created by Friedrich Schmidt on 26.05.19.
+//  Copyright © 2019 Universität Salzburg. All rights reserved.
+//
+
+#include "SteveDrawing.hpp"
