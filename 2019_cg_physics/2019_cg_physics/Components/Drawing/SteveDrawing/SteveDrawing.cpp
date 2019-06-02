7bfce2035103c05fa93776bc794f2b016aef0798
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2019-06-02 07:53:47 +0200

    Deleted unwanted files.

--- 2019_cg_physics/2019_cg_physics/Components/Drawing/SteveDrawing/SteveDrawing.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  SteveDrawing.cpp
-//  2019_cg_physics
-//
-//  Created by Friedrich Schmidt on 26.05.19.
-//  Copyright © 2019 Universität Salzburg. All rights reserved.
-//
-
-#include "SteveDrawing.hpp"
