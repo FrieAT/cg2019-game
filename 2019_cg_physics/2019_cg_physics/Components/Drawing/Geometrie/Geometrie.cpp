9d2c60cd651253c63c28fa5c0a6022ec9a957cc9
Mariya Davidouskaya <mariya.davidouskaya@stud.cosy.sbg.ac.at>
2019-06-11 18:49:26 +0200

    dreicks

---
+++ 2019_cg_physics/2019_cg_physics/Components/Drawing/Geometrie/Geometrie.cpp
@@ -0,0 +1,9 @@
+//
+//  Geometrie.cpp
+//  2019_cg_physics
+//
+//  Created by Mariya Davidouskaya on 04.06.19.
+//  Copyright © 2019 Universität Salzburg. All rights reserved.
+//
+
+#include "Geometrie.hpp"
---
+++ 2019_cg_physics/2019_cg_physics/Components/Drawing/Geometrie/Geometrie.hpp
@@ -0,0 +1,14 @@
+//
+//  Geometrie.hpp
+//  2019_cg_physics
+//
+//  Created by Mariya Davidouskaya on 04.06.19.
+//  Copyright © 2019 Universität Salzburg. All rights reserved.
+//
+
+#ifndef Geometrie_hpp
+#define Geometrie_hpp
+
+#include <stdio.h>
+
+#endif /* Geometrie_hpp */
