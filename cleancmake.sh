992d8755a0d17bd14b3aee86deea9ae47492e543
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2019-07-23 12:01:42 +0200

    Added glm includes for cmake, if it isnt installed.

--- cleancmake.sh
+++ cleancmake.sh
@@ -2,9 +2,9 @@
 rm -r ./CMakeFiles/ 2>/dev/null
 rm ./CMakeCache.txt 2>/dev/null
 rm ./cmake_install.cmake 2>/dev/null
-rm -r ./2019_cg_physics/src/CMakeFiles/ 2>/dev/null
-rm ./2019_cg_physics/src/cmake_install.cmake 2>/dev/null
-rm ./2019_cg_physics/src/CMakeCache.txt 2>/dev/null
+rm -r ./src/CMakeFiles/ 2>/dev/null
+rm ./src/cmake_install.cmake 2>/dev/null
+rm ./src/CMakeCache.txt 2>/dev/null
 rm ./Makefile 2>/dev/null
-rm ./2019_cg_physics/src/Makefile 2>/dev/null
-rm ./2019_cg_physics/src/2019_cg_physics
+rm ./src/Makefile 2>/dev/null
+rm ./src/2019_cg_physics
